cb20d8393b379572ffeac5801faa0f49f6fc6cb0
user <user@DESKTOP-UOGUC2Q>
2023-02-14 21:09:54 +0800

    file changed, lines 10-12

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -7,7 +7,9 @@ public class Library {
 	
 	/* Step 1.5*/
 	public static void main(String arg[]) {
-		System.out.println("Hello, world!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello, world!");
 	}
 	
 	
